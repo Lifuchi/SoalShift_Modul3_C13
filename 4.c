f4b646c82f4059c8bc624bb3a6df81d38d7f2bb0
Lifuchi <geminigirl124@gmail.com>
2017-10-24 09:38:59 -0700

    nomer 4 b

--- 4.c
+++ 4.c
@@ -14,14 +14,17 @@ typedef struct inputan{
 
 void* faktorial(void *arg)
 {
+	int i=0;
 	struct inputan *ngitung = (struct inputan*)arg;
     pthread_t id=pthread_self();
     if(pthread_equal(id,tid[0]))
     {
+	
     
     }
     else 
     {
+	pthread_equal(id,tid[angka.c++])
         
     }
     return NULL;
@@ -37,5 +40,14 @@ int main(void)
     }
     pthread_join(tid[0],NULL);
     pthread_join(tid[1],NULL);
+    pthread_join(tid[2],NULL);
+    pthread_join(tid[3],NULL);
+    pthread_join(tid[4],NULL);
+    pthread_join(tid[5],NULL);
+    pthread_join(tid[6],NULL);
+    pthread_join(tid[7],NULL);
+    pthread_join(tid[8],NULL);
+    pthread_join(tid[9],NULL);
+
     return 0;
 }
