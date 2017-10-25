47d82a54c3354fc7f28ceff096f4749808f5be22
Lifuchi <geminigirl124@gmail.com>
2017-10-24 22:45:31 -0700

    masih error

--- 5.c
+++ 5.c
@@ -10,10 +10,8 @@ pthread_t tid[2];
 
 
 typedef struct cari{
-	char n[1000];
-	char n2[1000];
+	char *n[1000];
 	int c;
-	int c2;
 }input;
 
 
@@ -24,7 +22,7 @@ void* mencari(void *arg)
    if(pthread_equal(id,tid[0])){
 	FILE *fnovel;
 	char ch;
-	char string[1000];
+	char *string[1000];
 	fnovel = fopen("Novel.txt", "r");
 	while  (!feof(fnovel)) {
 		fscanf(fnovel,"%s",string);	
@@ -35,32 +33,25 @@ void* mencari(void *arg)
 	printf("%s : %d\n",ngitung->n,ngitung->c);
 	
 	}
-	else if(pthread_equal(id,tid[1])){
 	
-	FILE *fnovel;
-	char ch;
-	char string[1000];
-	fnovel = fopen("Novel.txt", "r");
-	while  (!feof(fnovel)) {
-		fscanf(fnovel,"%s",string);	
-		if(strstr(string,ngitung->n2) != 0) ngitung->c2++;
-	
-		}
-	fclose(fnovel);
-	printf("%s : %d\n",ngitung->n2,ngitung->c2);
-	
-	}
 	return NULL;
 }
-int main(void)
+int main(int argc ,char *argv[])
 {
     int i=0;
     int err;
+	char temp[100];
     input nama;
-	scanf("%s",nama.n);
-	scanf("%s",nama.n2);
+    
+     while(i <= argc)
+    {	if(argv[i+1] == NULL) break;
+	nama.n[i] = argv[i+1];	
+	i++;  
+    }
+//	scanf("%s",nama.n);
+//	scanf("%s",nama.n2);
 	nama.c = 0;
-	nama.c2 = 0;
+//	nama.c2 = 0;
 	while (i <2 ){
 	err=pthread_create(&(tid[i]),NULL,&mencari,&nama);
 	i++;
