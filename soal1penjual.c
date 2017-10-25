bd0dabef943a88306710975d38aaad714ed3e35f
rafidahrafalaia <32997391+rafidahrafalaia@users.noreply.github.com>
2017-10-25 17:43:45 +0700

    Update soal1penjual.c

--- soal1penjual.c
+++ soal1penjual.c
@@ -24,20 +24,12 @@ char nama_barang[100];
 key_t KEY = ftok("#", 'A');
 int sharedID = shmget(sharedKEY, sizeof(struct Memory), IPC_CREAT | 0666);
 
-key_t KEY;
+//key_t KEY;
 int pilih;
 
-struct stock s;
 //char senjata[100];
 int total;
 s = (struct stock *) shmat(sharedID, NULL, 0);
-
-s.MP4A1=0;
-s.PM2_V1=0;
-s.SPR_3=0;
-s.SS2_V5=0;
-s.SPG1_V3=0;
-s.MINE=0;
 
 while(1)
 {
@@ -75,5 +67,39 @@ if (pilih == 1)
  printf("STOK SEMUA KOSONG\n\n");
 }
   
+else if(pilih == 2){
+scanf("%s %d", nama_barang, &total);
+
+if (strcmp(nama_barang,"MP4A1")==0){
+s->MP4A1 += total;
+printf("Jumlah stock MP4A1 = %d \n", s->MP4A1);
 }
 
+if (strcmp(nama_barang,"PM2-V1")==0){
+s->PM2_V1 += total;
+printf("Jumlah stock PM2-V1 = %d \n", s->PM2_V1);
+}
+
+if (strcmp(nama_barang,"SPR-3")==0){
+s->SPR_3 += tambahan;
+printf("Jumlah stock SPR-3 = %d \n", s->SPR_3);
+}
+
+if (strcmp(nama_barang,"SS2-V5")==0){
+s->SS2_V5 += total;
+printf("Jumlah stock SS2-V5 = %d \n", s->SS2_V5);
+}
+
+if (strcmp(nama_barang,"SPG1-V3")==0){
+s->SPG1_V3 += total;
+printf("Jumlah stock SPG1-V3 = %d \n", s->SPG1_V3);
+}
+
+if (strcmp(nama_barang,"MINE")==0){
+s->MINE += total;
+printf("Jumlah stock MINE = %d \n", s->MINE);
+}
+}
+
+}
+}
