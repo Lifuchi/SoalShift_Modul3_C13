8f0e4219efea322a0ad0f300bf3580045b184866
rafidahrafalaia <rafidahlia6@gmail.com>
2017-10-25 17:59:03 +0700

    fix

--- soal1penjual.c
+++ soal1penjual.c
@@ -18,18 +18,18 @@ int MINE;
 };
 
 int main(){
-struct stock *s;
+struct stock *data;
 char nama_barang[100];
 
 key_t KEY = ftok("#", 'A');
-int sharedID = shmget(sharedKEY, sizeof(struct Memory), IPC_CREAT | 0666);
+int sharedID = shmget(KEY, sizeof(struct stock), IPC_CREAT | 0666);
 
 //key_t KEY;
 int pilih;
 
 //char senjata[100];
 int total;
-s = (struct stock *) shmat(sharedID, NULL, 0);
+data = (struct stock *) shmat(sharedID, NULL, 0);
 
 while(1)
 {
@@ -39,65 +39,63 @@ scanf("%d", &pilih);
 
 if (pilih == 1)
 {
- if(s->MP4A1>0){
+ if(data->MP4A1>0){
  printf("Jumlah MP4A1 = %d\n", data->MP4A1);
  }
 
- if(s->PM2_V1>0){
+ if(data->PM2_V1>0){
  printf("Jumlah PM2-V1 = %d\n", data->PM2_V1);
  }
 
- if(s->SPR_3>0){
+ if(data->SPR_3>0){
  printf("Jumlah SPR-3 = %d\n", data->SPR_3);
  }
 
- if(s->SS2_V5>0){
+ if(data->SS2_V5>0){
  printf("Jumlah SS2-V5 = %d\n", data->SS2_V5);
  }
 
- if(s->SPG1_V3){
+ if(data->SPG1_V3>0){
  printf("Jumlah SPG1-V3 = %d\n", data->SPG1_V3);
  }
 
- if(s->MINE){
+ if(data->MINE>0){
  printf("Jumlah MINE = %d\n", data->MINE);
  }
-
- else{
- printf("STOK SEMUA KOSONG\n\n");
-}
-  
+else
+printf("stock kosong");
+}  
 else if(pilih == 2){
 scanf("%s %d", nama_barang, &total);
 
 if (strcmp(nama_barang,"MP4A1")==0){
-s->MP4A1 += total;
-printf("Jumlah stock MP4A1 = %d \n", s->MP4A1);
+data->MP4A1 += total;
+printf("Jumlah stock MP4A1 = %d \n", data->MP4A1);
 }
 
 if (strcmp(nama_barang,"PM2-V1")==0){
-s->PM2_V1 += total;
-printf("Jumlah stock PM2-V1 = %d \n", s->PM2_V1);
+data->PM2_V1 += total;
+printf("Jumlah stock PM2-V1 = %d \n", data->PM2_V1);
 }
 
 if (strcmp(nama_barang,"SPR-3")==0){
-s->SPR_3 += tambahan;
-printf("Jumlah stock SPR-3 = %d \n", s->SPR_3);
+data->SPR_3 += total;
+printf("Jumlah stock SPR-3 = %d \n", data->SPR_3);
 }
 
 if (strcmp(nama_barang,"SS2-V5")==0){
-s->SS2_V5 += total;
-printf("Jumlah stock SS2-V5 = %d \n", s->SS2_V5);
+data->SS2_V5 += total;
+printf("Jumlah stock SS2-V5 = %d \n", data->SS2_V5);
 }
 
 if (strcmp(nama_barang,"SPG1-V3")==0){
-s->SPG1_V3 += total;
-printf("Jumlah stock SPG1-V3 = %d \n", s->SPG1_V3);
+data->SPG1_V3 += total;
+printf("Jumlah stock SPG1-V3 = %d \n", data->SPG1_V3);
 }
 
 if (strcmp(nama_barang,"MINE")==0){
-s->MINE += total;
-printf("Jumlah stock MINE = %d \n", s->MINE);
+data->MINE += total;
+printf("Jumlah stock MINE = %d \n", data->MINE);
 }
 }
 
