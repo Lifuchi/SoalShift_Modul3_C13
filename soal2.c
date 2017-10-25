53b97be67823e2134e92e80f89a117a80e724a52
Lifuchi <geminigirl124@gmail.com>
2017-10-25 03:39:07 -0700

    hmm

--- soal2.c
+++ soal2.c
@@ -7,6 +7,7 @@
 pthread_t tid1;
 pthread_t tid2;
 int status;
+int status2;
 
 typedef struct player{
 	int mine[17];
@@ -20,9 +21,13 @@ void* pemain1(void *arg)
 {
     struct player *maen =  (struct player*)arg;
     char t;
+    while(status != 0 )
+    {
+
+    }
     status = 0;	
     int flag = 4;
-printf("\nPlayer 1 silahkan memilih angka untuk meletakkan ranjau sebanyak 1-4 \n");
+    printf("\nPlayer 1 silahkan memilih angka untuk meletakkan ranjau sebanyak 1-4 \n");
     while(flag != 0){
 	scanf("%d",&maen->lub,&t);
 	maen->mine[maen->lub] = 1;
@@ -30,11 +35,7 @@ printf("\nPlayer 1 silahkan memilih angka untuk meletakkan ranjau sebanyak 1-4 \
     }
     printf("ranjau sudah dipasang\n");
     status = 1;
-    while(status != 1)
-    {
-
-    }
-    printf("Player 2 silahkan memilih 4 lubang\n");
+    printf("Player 1 silahkan memilih 4 lubang\n");
     int i, j ;
     int x;
 	for (i = 0 ; i < 4 ; i++)
@@ -43,7 +44,8 @@ printf("\nPlayer 1 silahkan memilih angka untuk meletakkan ranjau sebanyak 1-4 \
 		if(maen->mine[x] == 1) {printf("anda benar\n");maen->poin += 1;}
 		else {printf("anda salah\n");} 	
 	}
-
+	printf("poin player 1 : %d\n",maen->poin);
+	status = 1;
     return NULL;
 }
 
@@ -51,12 +53,32 @@ printf("\nPlayer 1 silahkan memilih angka untuk meletakkan ranjau sebanyak 1-4 \
 void* pemain2(void *arg)
 {
     struct player *maen2 =  (struct player*)arg;
-    while(status != 1)
+    while(status != 1 )
     {
 
     }
-    
-	
+    status = 0;
+    char t;
+    int flag = 4;
+    printf("\nPlayer 2 silahkan memilih angka untuk meletakkan ranjau sebanyak 1-4 \n");
+    while(flag != 0){
+	scanf("%d",&maen2->lub,&t);
+	maen2->mine[maen2->lub] = 1;
+	flag--; 
+    }
+    printf("ranjau sudah dipasang\n");
+    printf("Player 2 silahkan memilih 4 lubang\n");
+    int i, j ;
+    int x;
+	for (i = 0 ; i < 4 ; i++)
+	{
+		scanf("%d", &x);
+		if(maen2->mine[x] == 1) {printf("anda benar\n");maen2->poin += 1;}
+		else {printf("anda salah\n");} 	
+	}
+	printf("poin player 2 : %d\n",maen2->poin);
+    status = 1;
+return 0;	
     
 }
  
@@ -67,6 +89,7 @@ int main(void)
     plyr2.poin = 0;
     memset(plyr1.mine , 0 , 16);
     memset(plyr2.mine, 0 , 16);
+    status = 1;
     
     pthread_create(&(tid1), NULL, &pemain1, &plyr1);
     pthread_create(&(tid2), NULL, &pemain2, &plyr2);
