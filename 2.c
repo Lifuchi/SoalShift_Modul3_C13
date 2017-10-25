c6e521f48723fc37a27fd0c34286174f0745ca69
Lifuchi <geminigirl124@gmail.com>
2017-10-25 02:51:19 -0700

    lol

--- 2.c
+++ 2.c
@@ -11,6 +11,7 @@ int status;
 typedef struct player{
 	int mine[16];
 	int poin;
+	int lub;
 }pmn;
 
 
@@ -18,7 +19,19 @@ typedef struct player{
 void* pemain1(void *arg)
 {
     struct player *maen =  (struct player*)arg;
-    status = 1;
+    char t;
+    status = 1;	
+    int flag = 0;
+
+    while(flag < 4){
+	printf("\nPlayer 1 silahkan memilih angka untuk meletakkan ranjau sebanyak 1-4 \n");
+	scanf("%d%c",&maen->lub,&t);
+	maen->mine[maen->lub] = 1;
+	flag++;
+	if(t == '\n') return 0; 
+	
+    }
+	status = 0;
     return NULL;
 }
 
@@ -38,11 +51,14 @@ int main(void)
     pmn plyr1,plyr2;
     plyr1.poin = 0;
     plyr2.poin = 0;
-	
-
+    memset(plyr1.mine , 0 , 16);
+    memset(plyr2.mine, 0 , 16);
+    
     pthread_create(&(tid1), NULL, &pemain1, &plyr1);
     pthread_create(&(tid2), NULL, &pemain2, &plyr2);
  
+    printf("poin pemain 1 = \n",plyr1.poin );
+    printf("poin pemain 2 = \n",plyr2.poin );
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
  
