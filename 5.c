8996ed14882dc309a8146f4d12ba05d47eb8fa1d
Lifuchi <geminigirl124@gmail.com>
2017-10-24 22:56:23 -0700

    tambah hancur

--- 5.c
+++ 5.c
@@ -7,11 +7,12 @@
 #include<ctype.h>
 
 pthread_t tid[2];
-
+char *n[100];
 
 typedef struct cari{
-	char *n[1000];
+	char *n2[100];
 	int c;
+	int t;
 }input;
 
 
@@ -19,45 +20,46 @@ void* mencari(void *arg)
 {
   struct cari *ngitung =  (struct cari*)arg;
 	pthread_t id= pthread_self();
-   if(pthread_equal(id,tid[0])){
+	int b = 0;
+	for (b = 0 ; b <= ngitung->t ; b++ ){
+ 	  if(pthread_equal(id,tid[b])){
 	FILE *fnovel;
 	char ch;
-	char *string[1000];
+	char string[1000];
 	fnovel = fopen("Novel.txt", "r");
+
 	while  (!feof(fnovel)) {
 		fscanf(fnovel,"%s",string);	
-		if(strstr(string,ngitung->n) != 0) ngitung->c++;
-	
+		if(strstr(string,n[b]) != 0) ngitung->c++;
 		}
 	fclose(fnovel);
-	printf("%s : %d\n",ngitung->n,ngitung->c);
-	
+	printf("%s : %d\n",n[b],ngitung->c);
+	}
 	}
 	
 	return NULL;
 }
 int main(int argc ,char *argv[])
 {
-    int i=0;
+    int i=0, j =0;
     int err;
 	char temp[100];
     input nama;
     
-     while(i <= argc)
+     while(1)
     {	if(argv[i+1] == NULL) break;
-	nama.n[i] = argv[i+1];	
+	nama.n2[i] = argv[i+1];	
 	i++;  
     }
-//	scanf("%s",nama.n);
-//	scanf("%s",nama.n2);
+	nama.t = i;
 	nama.c = 0;
-//	nama.c2 = 0;
-	while (i <2 ){
-	err=pthread_create(&(tid[i]),NULL,&mencari,&nama);
-	i++;
+	while (j < 2 ){
+	err=pthread_create(&(tid[j]),NULL,&mencari,&nama);
+	j++;
 	}
 
     pthread_join(tid[0],NULL);
     pthread_join(tid[1],NULL);
+
     return 0;
 }
